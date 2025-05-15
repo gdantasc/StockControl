244929682558dbd0c482d093612c6eb12eed4382
gdantasc <gabrieldcavalcante00@gmail.com>
2025-05-15 15:19:36 -0300

    feat: create category methods

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -30,4 +30,15 @@ export class CategoriesService {
       this.httpOptions
     )
   }
+
+  deleteCategory(requestDatas: { category_id: string }): Observable<void> {
+    return this.httpClient.delete<void>(
+      `${this.API_URL}/category/delete`, {
+      ...this.httpOptions,
+      params: {
+        category_id: requestDatas.category_id
+      }
+    }
+    )
+  }
 }
